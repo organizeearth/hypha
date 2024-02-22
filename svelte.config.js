434f7a37b50cf55c23db80d4462946c6fe601728
organizeearth <153849390+organizeearth@users.noreply.github.com>
2024-02-22 11:45:19 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,6 @@ export default {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			fallback: 'index.html',
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
